5cec3ab54a915364ad19c7be68debaf6a563ee68
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-02-15 11:36:31 +0700

    Move routes into separate file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const logger = require('./middleware/logger');
-const members = require("./Members");
 
 const app = express();
 
@@ -18,20 +17,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Init middleware
 app.use(logger);
 
-// Create simple REST API
-// Get all members
-app.get("/api/members", (req, res) => res.json(members));
-
-// Get single member
-app.get('/api/members/:id', (req, res) => {
-  const found = members.some((member) => member.id === parseInt(req.params.id));
-  if (found) {
-    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({
-      msg: `No member with the id of ${req.params.id}`
-    });
-  }
-})
+// Members API routes
+app.use('/api/members', require('./routes/api/members'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
